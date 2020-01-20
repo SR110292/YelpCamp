1e3310371fa3322a5866d454c134832451171173
SR110292 <suryafromindia@gmail.com>
2020-01-20 20:09:04 +0530

    new upate from surya

--- src/main/java/resources/BrowserOnAndOff.java
+++ src/main/java/resources/BrowserOnAndOff.java
@@ -26,6 +26,7 @@ import pages.WelcomePage;
 //changes made in develop of git codes
 //changes made in develop of Surya
 //changes made in develop of git codes
+//changes made in master of Surya
 public class BrowserOnAndOff {
 	public static ChromeDriver driver;
 	public static String VCNsheet;
